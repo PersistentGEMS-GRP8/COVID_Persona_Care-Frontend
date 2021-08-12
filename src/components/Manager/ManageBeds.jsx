9e7cea690ac6a29357d12e49694756e61ec0f07f
Sachini Chathurangi <sachini_chathurangi@persistent.com>
2021-08-12 14:05:54 +0530

    manage beds

--- src/components/Manager/ManageBeds.jsx
+++ src/components/Manager/ManageBeds.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import Navbar from '../layout/navbar';
 
 class ManageBeds extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quantity: 10
+        }
+    }
+
+    incrementBeds = () => {
+        this.setState({ quantity: this.state.quantity + 1 });
+      }
+      decreaseBeds = () => {
+        this.setState({ quantity: this.state.quantity - 1 });
+      }
     render() {
         return (
             <div>
@@ -14,22 +27,22 @@ class ManageBeds extends React.Component {
                         <div class="col-lg-2">
                             <div class="input-group">
                                 <span class="input-group-btn">
-                                    <button type="button" class="quantity-left-minus btn btn-danger btn-number" data-type="minus" data-field="">
-                                        <span class="glyphicon glyphicon-minus"></span>
+                                    <button type="button" class=" btn btn-danger" onClick={this.decreaseBeds}>
+                                        -1
                                     </button>
                                 </span>
-                                <input type="text" id="quantity" name="quantity" class="form-control input-number" value="10" min="1" max="100"/>
-                                    <span class="input-group-btn">
-                                        <button type="button" class="quantity-right-plus btn btn-success btn-number" data-type="plus" data-field="">
-                                            <span class="glyphicon glyphicon-plus"></span>
-                                        </button>
-                                    </span>
-                                </div>
+                                <input type="text" id="quantity" name="quantity" class="form-control input-number" value={this.state.quantity} min="1" max="100" />
+                                <span class="input-group-btn">
+                                    <button type="button" class=" btn btn-success" onClick={this.incrementBeds}>
+                                        +1
+                                    </button>
+                                </span>
                             </div>
                         </div>
                     </div>
                 </div>
-                )
+            </div>
+        )
     }
 }
-                export default ManageBeds;
+export default ManageBeds;
