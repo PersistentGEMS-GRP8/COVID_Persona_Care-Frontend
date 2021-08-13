08a911b901104f3d4ac68cf8c0c548fcde233f2c
Dilsha Hettiarachchi <dilshanavodi@gmail.com>
2021-08-13 13:48:23 +0530

    remove commented lines

--- src/services/ManagerService.js
+++ src/services/ManagerService.js
@@ -1,7 +1,5 @@
 import http from './httpService';
 
-// const MANAGER_API_BASE_URL = "http://localhost:8088/COVIDPersonaCare/managers";
-
 class ManagerService {
 
     getManagersByHId(hId){
@@ -26,35 +24,3 @@ class ManagerService {
 }
 
 export default new ManagerService()
-
-// export default new ManagerService()
-
-// import axios from 'axios';
-// import authHeader from './auth-header';
-
-// const MANAGER_API_BASE_URL = "http://localhost:8088/COVIDPersonaCare/managers";
-
-// class ManagerService {
-
-//     getManagersByHId(hId){
-//         return axios.get(MANAGER_API_BASE_URL + '/getByHId/' + hId, { headers: authHeader() });
-//     }
-
-//     createManager(manager){
-//         return axios.post(MANAGER_API_BASE_URL + '/register', manager);
-//     }
-
-//     getManagerById(managerId){
-//         return axios.get(MANAGER_API_BASE_URL + '/' + managerId);
-//     }
-
-//     updateManager(manager){
-//         return axios.put(MANAGER_API_BASE_URL, manager);
-//     }
-
-//     deleteManager(managerId){
-//         return axios.delete(MANAGER_API_BASE_URL + '/' + managerId);
-//     }
-// }
-
-// export default new ManagerService()
--- src/services/ReceptionistService.js
+++ src/services/ReceptionistService.js
@@ -1,7 +1,5 @@
 import http from './httpService';
 
-// const RECEPTIONIST_API_BASE_URL = "http://localhost:8088/COVIDPersonaCare/receptionists";
-
 class ReceptionistService {
 
     getReceptionistsByHId(hId){
@@ -26,32 +24,3 @@ class ReceptionistService {
 }
 
 export default new ReceptionistService()
-
-// import axios from 'axios';
-
-// const RECEPTIONIST_API_BASE_URL = "http://localhost:8088/COVIDPersonaCare/receptionists";
-
-// class ReceptionistService {
-
-//     getReceptionistsByHId(hId){
-//         return axios.get(RECEPTIONIST_API_BASE_URL + '/getByHId/' + hId);
-//     }
-
-//     createReceptionist(receptionist){
-//         return axios.post(RECEPTIONIST_API_BASE_URL + '/register', receptionist);
-//     }
-
-//     getReceptionistById(receptionistId){
-//         return axios.get(RECEPTIONIST_API_BASE_URL + '/' + receptionistId);
-//     }
-
-//     updateReceptionist(receptionist){
-//         return axios.put(RECEPTIONIST_API_BASE_URL, receptionist);
-//     }
-
-//     deleteReceptionist(receptionistId){
-//         return axios.delete(RECEPTIONIST_API_BASE_URL + '/' + receptionistId);
-//     }
-// }
-
-// export default new ReceptionistService()
