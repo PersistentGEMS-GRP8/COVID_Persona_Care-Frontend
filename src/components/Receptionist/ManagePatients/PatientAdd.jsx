abc7ce1bd6fd2f3828932db0ef6d089e6aeb1e6c
sulakshana_ranawake <sula.ranawake@gmail.com>
2021-08-15 02:44:42 +0530

    Patient Add Form - submit function updated

--- src/components/Receptionist/ManagePatients/PatientAdd.jsx
+++ src/components/Receptionist/ManagePatients/PatientAdd.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PatientService from '../../../services/PatientService';
 import Navbar from '../../layout/navbar';
 
 export default class PatientAdd extends Component {
@@ -7,13 +8,43 @@ export default class PatientAdd extends Component {
     constructor(props) {
         super(props);
         this.cancel = this.cancel.bind(this);
+
+        this.state = {
+            name: '',
+            email: '',
+            contact: '',
+            vStatus: ''
+        }
     }
 
-    cancel(){
+    cancel() {
         this.props.history.push("/patient/list");
     }
 
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+
+        const {patient} = this.state;
+
+        try {
+            PatientService.createPatient(patient).then(() => this.props.history.push("/patient/list"));
+        } catch (error) {
+            console.log("Data could not be stored ",error);
+        }
+        
+        
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        });
+    }
+
     render() {
+        const {name, email, contact, vStatus} = this.state;
+
         return (
             <div>
                 <Navbar/> 
@@ -30,32 +61,32 @@ export default class PatientAdd extends Component {
                         <div className="mb-3 row">
                             <label className="col-sm-2 col-form-label">Name</label>
                             <div className="col-sm-10">
-                                <input className="form-control" name="name" id="name" placeholder="Jane Doe"/>
+                                <input className="form-control" name="name" id="name" type="text" value={name} onChange={this.handleChange} placeholder="Jane Doe"/>
                             </div>
                             
                         </div>
                         <div className="mb-3 row">
                             <label className="col-sm-2 col-form-label">Email</label>
                             <div className="col-sm-10">
-                                <input className="form-control" name="email" id="email" placeholder="abc@testmail.com"/>
+                                <input className="form-control" name="email" id="email" value={email} onChange={this.handleChange}  placeholder="abc@testmail.com"/>
                             </div>
                         </div>
                         <div className="mb-3 row">
                             <label className="col-sm-2 col-form-label">Contact number</label>
                             <div className="col-sm-10">
-                                <input className="form-control" name="contact" id="contact" placeholder="+91XXXXXXXXXX"/>
+                                <input className="form-control" name="contact" id="contact" value={contact} onChange={this.handleChange}  placeholder="+91XXXXXXXXXX"/>
                             </div>
                         </div>
                         <div className="mb-3 row">
                             <label className="col-sm-2 col-form-label">Vaccination Status</label>
                             <div className="col-sm-10">
-                                <input className="form-control" name="vStatus" id="vStatus" placeholder="YES/NO"/>
+                                <input className="form-control" name="vStatus" id="vStatus" value={vStatus} onChange={this.handleChange} placeholder="YES/NO"/>
                             </div>
                         </div>
                         <br></br>
                         <div className="mb-3 row">
                             <div className="col text-center">
-                            <button className="btn button-custom me-2" type="submit">Register</button>
+                            <button className="btn button-custom me-2" type="submit" onClick={this.handleSubmit}>Register</button>
                             <button className="btn btn-danger ms-2" onClick={this.cancel}>Cancel</button>
                             </div>
                         </div>
