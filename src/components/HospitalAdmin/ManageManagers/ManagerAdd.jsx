8a0036086d644a28d3d117578f41c804f8e11b1b
Dilsha Hettiarachchi <dilshanavodi@gmail.com>
2021-08-15 09:29:20 +0530

    receptionist and manager registration & edit forms validation

--- src/components/HospitalAdmin/ManageManagers/ManagerAdd.jsx
+++ src/components/HospitalAdmin/ManageManagers/ManagerAdd.jsx
@@ -6,64 +6,161 @@ import HospitalAdminNavbar from '../HospitalAdminNavbar';
 class ManagerAdd extends Component {
 
   emptyManager = {
-    personaUser:{
+    personaUser: {
       username: '',
       password: '',
       role: 'ROLE_MANAGER',
     },
-    person:{
-      type:'manager',
+    person: {
+      type: 'manager',
       name: '',
       email: '',
       contactNo: '',
-      hId:2
+      hId: 2
     }
-    
+
   }
 
   constructor(props) {
     super(props);
     this.state = {
-      manager: this.emptyManager
+      manager: this.emptyManager,
+      errors: [],
+      errorMsgs: {
+        nameError: '',
+        emailError: '',
+        contactNoError: '',
+        usernameError: ''
+      }
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.cancel = this.cancel.bind(this);
   }
 
+  hasError(key) {
+    return this.state.errors.indexOf(key) !== -1;
+  }
+
   handleChange(event) {
     const target = event.target;
     const value = target.value;
     const name = target.name;
-    let manager = {...this.state.manager};
+    let manager = { ...this.state.manager };
     manager[name] = value;
     manager.person[name] = value;
     manager.personaUser[name] = value;
-    this.setState({manager});
+    this.setState({ manager });
+  }
+
+  validate() {
+    const { manager, errorMsgs } = this.state;
+    var errors = [];
+
+    //name
+    if (manager.person.name === "") {
+      errors.push("name");
+      errorMsgs.nameError = 'Please provide a name.'
+    } else if (manager.person.name.length < 3) {
+      errors.push("name");
+      errorMsgs.nameError = 'Name should be atleast 3 characters.'
+    }
+
+    //email
+    if (manager.person.email === "") {
+      errors.push("email");
+      errorMsgs.emailError = 'Please provide a email.'
+    } else {
+      const expression = /\S+@\S+/;
+      var validEmail = expression.test(String(manager.person.email).toLowerCase());
+
+      if (!validEmail) {
+        errors.push("email");
+        errorMsgs.emailError = 'Please provide a valid email.'
+      }
+
+    }
+    //contact No
+    if (manager.person.contactNo === "") {
+      errors.push("contactNo");
+      errorMsgs.contactNoError = 'Please provide a contact No.'
+    } else if ((manager.person.contactNo.length != 10) && (manager.person.contactNo.length != 12)) {
+      errors.push("contactNo");
+      errorMsgs.contactNoError = 'Contact no should contain 10 or 12 characters.'
+    }
+
+    //username
+    if (manager.personaUser.username === "") {
+      errors.push("username");
+      errorMsgs.usernameError = 'Please provide a username';
+    }
+
+    //password
+    if (manager.personaUser.password === "") {
+      errors.push("password");
+    }
+
+    this.setState({
+      errors: errors,
+      errorMsgs: errorMsgs
+    });
+
+    if (errors.length > 0) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    const {manager} = this.state;
+    const { manager } = this.state;
+    var msgs = {
+      nameError: '',
+      emailError: '',
+      contactNoError: '',
+      usernameError: ''
+    }
+    this.setState({
+      errors: [],
+      errorMsgs: msgs
+    }, () => {
+      if (this.validate()) {
+        console.log(manager);
+        ManagerService.createManager(manager).then(res => {
+          this.props.history.push('/managers');
+        }).catch(function (error) {
+            //handle error 
+            // console.log(error.response.status);
+            // console.log(error.response.data.message);
+            if (error.response.status == 400) {
+              const { errorMsgs, errors } = this.state;
+              errors.push("username");
+              errorMsgs.usernameError = 'Username already exists.';
+              this.setState({
+                errors: errors,
+                errorMsgs: errorMsgs
+              });
+            }
 
-    console.log(manager);
-    ManagerService.createManager(manager).then(res =>{
-        this.props.history.push('/managers');
+          }.bind(this));
+      }
     });
-    
+
+
   }
 
-  cancel(){
+  cancel() {
     this.props.history.push('/managers');
   }
 
   render() {
-    const {manager} = this.state;
+    const { manager, errorMsgs } = this.state;
     const title = <h2>{'Manager Details'}</h2>;
 
     return <div>
-      <HospitalAdminNavbar/> 
-      <br/>
+      <HospitalAdminNavbar />
+      <br />
       <div className="container">
         {title}
         <br></br>
@@ -71,43 +168,63 @@ class ManagerAdd extends Component {
           <div className="mb-3 row">
             <label htmlFor="name" className="col-sm-2 col-form-label">Name</label>
             <div className="col-sm-10">
-              <input type="text" className="form-control" name="name" id="name" value={manager.person.name}
-                    onChange={this.handleChange} autoComplete="name"/>
+              <input type="text" className={this.hasError("name") ? "form-control is-invalid" : "form-control"}
+                name="name" id="name" value={manager.person.name}
+                onChange={this.handleChange} autoComplete="name" />
+              <div class="invalid-feedback">
+                {errorMsgs.nameError}
+              </div>
             </div>
           </div>
           <div className="mb-3 row">
             <label htmlFor="email" className="col-sm-2 col-form-label">Email</label>
             <div className="col-sm-10">
-              <input type="text" className="form-control" name="email" id="email" value={manager.person.email}
-                   onChange={this.handleChange} autoComplete="email"/>
+              <input type="text" className={this.hasError("email") ? "form-control is-invalid" : "form-control"}
+                name="email" id="email" value={manager.person.email}
+                onChange={this.handleChange} autoComplete="email" />
+              <div class="invalid-feedback">
+                {errorMsgs.emailError}
+              </div>
             </div>
           </div>
           <div className="mb-3 row">
             <label htmlFor="contactNo" className="col-sm-2 col-form-label">Contact No</label>
             <div className="col-sm-10">
-              <input type="text" className="form-control" name="contactNo" id="contactNo" value={manager.person.contactNo}
-                   onChange={this.handleChange} autoComplete="contactNo"/>
+              <input type="text" className={this.hasError("contactNo") ? "form-control is-invalid" : "form-control"}
+                name="contactNo" id="contactNo" value={manager.person.contactNo}
+                onChange={this.handleChange} autoComplete="contactNo" />
+              <div class="invalid-feedback">
+                {errorMsgs.contactNoError}
+              </div>
             </div>
           </div>
           <div className="mb-3 row">
             <label htmlFor="username" className="col-sm-2 col-form-label">Username</label>
             <div className="col-sm-10">
-              <input type="text" className="form-control" name="username" id="username" value={manager.personaUser.username}
-                    onChange={this.handleChange} autoComplete="username"/>
+              <input type="text" className={this.hasError("username") ? "form-control is-invalid" : "form-control"}
+                name="username" id="username" value={manager.personaUser.username}
+                onChange={this.handleChange} autoComplete="username" />
+              <div class="invalid-feedback">
+                {errorMsgs.usernameError}
+              </div>
             </div>
           </div>
           <div className="mb-3 row">
             <label htmlFor="password" className="col-sm-2 col-form-label">Default Password</label>
             <div className="col-sm-10">
-              <input type="text" className="form-control" name="password" id="password" value={manager.personaUser.password}
-                    onChange={this.handleChange} autoComplete="password"/>
+              <input type="text" className={this.hasError("password") ? "form-control is-invalid" : "form-control"}
+                name="password" id="password" value={manager.personaUser.password}
+                onChange={this.handleChange} autoComplete="password" />
+              <div class="invalid-feedback">
+                Please provide a password.
+              </div>
             </div>
           </div>
           <br></br>
           <div className="mb-3 row">
             <div className="col text-center">
-            <button className="btn button-custom me-2" type="submit">Register</button>
-            <button className="btn btn-secondary ms-2" onClick={this.cancel}>Cancel</button>
+              <button className="btn button-custom me-2" type="submit">Register</button>
+              <button className="btn btn-secondary ms-2" onClick={this.cancel}>Cancel</button>
             </div>
           </div>
         </form>
--- src/components/HospitalAdmin/ManageReceptionists/ReceptionistAdd.jsx
+++ src/components/HospitalAdmin/ManageReceptionists/ReceptionistAdd.jsx
@@ -6,63 +6,160 @@ import HospitalAdminNavbar from '../HospitalAdminNavbar';
 class ReceptionistAdd extends Component {
 
   emptyReceptionist = {
-    personaUser:{
+    personaUser: {
       username: '',
       password: '',
       role: 'ROLE_RECEPTIONIST',
     },
-    person:{
-      type:'receptionist',
+    person: {
+      type: 'receptionist',
       name: '',
       email: '',
       contactNo: '',
-      hId:2
+      hId: 2
     }
-    
+
   }
 
   constructor(props) {
     super(props);
     this.state = {
-      receptionist: this.emptyReceptionist
+      receptionist: this.emptyReceptionist,
+      errors: [],
+      errorMsgs: {
+        nameError: '',
+        emailError: '',
+        contactNoError: '',
+        usernameError: ''
+      }
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.cancel = this.cancel.bind(this);
   }
 
+  hasError(key) {
+    return this.state.errors.indexOf(key) !== -1;
+  }
+
   handleChange(event) {
     const target = event.target;
     const value = target.value;
     const name = target.name;
-    let receptionist = {...this.state.receptionist};
+    let receptionist = { ...this.state.receptionist };
     receptionist[name] = value;
     receptionist.person[name] = value;
     receptionist.personaUser[name] = value;
-    this.setState({receptionist});
+    this.setState({ receptionist });
+  }
+
+  validate() {
+    const { receptionist, errorMsgs } = this.state;
+    var errors = [];
+
+    //name
+    if (receptionist.person.name === "") {
+      errors.push("name");
+      errorMsgs.nameError = 'Please provide a name.'
+    } else if (receptionist.person.name.length < 3) {
+      errors.push("name");
+      errorMsgs.nameError = 'Name should be atleast 3 characters.'
+    }
+
+    //email
+    if (receptionist.person.email === "") {
+      errors.push("email");
+      errorMsgs.emailError = 'Please provide a email.'
+    } else {
+      const expression = /\S+@\S+/;
+      var validEmail = expression.test(String(receptionist.person.email).toLowerCase());
+
+      if (!validEmail) {
+        errors.push("email");
+        errorMsgs.emailError = 'Please provide a valid email.'
+      }
+
+    }
+    //contact No
+    if (receptionist.person.contactNo === "") {
+      errors.push("contactNo");
+      errorMsgs.contactNoError = 'Please provide a contact No.'
+    } else if ((receptionist.person.contactNo.length != 10) && (receptionist.person.contactNo.length != 12)) {
+      errors.push("contactNo");
+      errorMsgs.contactNoError = 'Contact no should contain 10 or 12 characters.'
+    }
+
+    //username
+    if (receptionist.personaUser.username === "") {
+      errors.push("username");
+      errorMsgs.usernameError = 'Please provide a username';
+    }
+
+    //password
+    if (receptionist.personaUser.password === "") {
+      errors.push("password");
+    }
+
+    this.setState({
+      errors: errors,
+      errorMsgs: errorMsgs
+    });
+
+    if (errors.length > 0) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    const {receptionist} = this.state;
-    console.log(receptionist);
-    ReceptionistService.createReceptionist(receptionist).then(res =>{
-        this.props.history.push('/receptionists');
-    });
-    
+    const { receptionist } = this.state;
+    var msgs = {
+      nameError: '',
+      emailError: '',
+      contactNoError: '',
+      usernameError: ''
+    }
+    this.setState({
+      errors: [],
+      errorMsgs: msgs
+    }, () => {
+      if (this.validate()) {
+        console.log(receptionist);
+        ReceptionistService.createReceptionist(receptionist).then(res => {
+          this.props.history.push('/receptionists');
+        }).catch(function (error) {
+          //handle error 
+          // console.log(error.response.status);
+          // console.log(error.response.data.message);
+          if (error.response.status == 400) {
+            const { errorMsgs, errors } = this.state;
+            errors.push("username");
+            errorMsgs.usernameError = 'Username already exists.';
+            this.setState({
+              errors: errors,
+              errorMsgs: errorMsgs
+            });
+          }
+
+        }.bind(this));
+      }
+    });;
+
   }
 
-  cancel(){
+  cancel() {
     this.props.history.push('/receptionists');
   }
 
   render() {
-    const {receptionist} = this.state;
+    const { receptionist, errorMsgs } = this.state;
     const title = <h2>{'Receptionist Details'}</h2>;
 
     return <div>
-      <HospitalAdminNavbar/> 
-      <br/>
+      <HospitalAdminNavbar />
+      <br />
       <div className="container">
         {title}
         <br></br>
@@ -70,43 +167,58 @@ class ReceptionistAdd extends Component {
           <div className="mb-3 row">
             <label htmlFor="name" className="col-sm-2 col-form-label">Name</label>
             <div className="col-sm-10">
-              <input type="text" className="form-control" name="name" id="name" value={receptionist.person.name}
-                    onChange={this.handleChange} autoComplete="name"/>
+              <input type="text" className={this.hasError("name") ? "form-control is-invalid" : "form-control"} name="name" id="name" value={receptionist.person.name}
+                onChange={this.handleChange} autoComplete="name" />
+              <div class="invalid-feedback">
+                {errorMsgs.nameError}
+              </div>
             </div>
           </div>
           <div className="mb-3 row">
             <label htmlFor="email" className="col-sm-2 col-form-label">Email</label>
             <div className="col-sm-10">
-              <input type="text" className="form-control" name="email" id="email" value={receptionist.person.email}
-                   onChange={this.handleChange} autoComplete="email"/>
+              <input type="text" className={this.hasError("email") ? "form-control is-invalid" : "form-control"} name="email" id="email" value={receptionist.person.email}
+                onChange={this.handleChange} autoComplete="email" />
+              <div class="invalid-feedback">
+                {errorMsgs.emailError}
+              </div>
             </div>
           </div>
           <div className="mb-3 row">
             <label htmlFor="contactNo" className="col-sm-2 col-form-label">Contact No</label>
             <div className="col-sm-10">
-              <input type="text" className="form-control" name="contactNo" id="contactNo" value={receptionist.person.contactNo}
-                   onChange={this.handleChange} autoComplete="contactNo"/>
+              <input type="text" className={this.hasError("contactNo") ? "form-control is-invalid" : "form-control"} name="contactNo" id="contactNo" value={receptionist.person.contactNo}
+                onChange={this.handleChange} autoComplete="contactNo" />
+              <div class="invalid-feedback">
+                {errorMsgs.contactNoError}
+              </div>
             </div>
           </div>
           <div className="mb-3 row">
             <label htmlFor="username" className="col-sm-2 col-form-label">Username</label>
             <div className="col-sm-10">
-              <input type="text" className="form-control" name="username" id="username" value={receptionist.personaUser.username}
-                    onChange={this.handleChange} autoComplete="username"/>
+              <input type="text" className={this.hasError("username") ? "form-control is-invalid" : "form-control"} name="username" id="username" value={receptionist.personaUser.username}
+                onChange={this.handleChange} autoComplete="username" />
+              <div class="invalid-feedback">
+                {errorMsgs.usernameError}
+              </div>
             </div>
           </div>
           <div className="mb-3 row">
             <label htmlFor="password" className="col-sm-2 col-form-label">Default Password</label>
             <div className="col-sm-10">
-              <input type="text" className="form-control" name="password" id="password" value={receptionist.personaUser.password}
-                    onChange={this.handleChange} autoComplete="password"/>
+              <input type="text" className={this.hasError("password") ? "form-control is-invalid" : "form-control"} name="password" id="password" value={receptionist.personaUser.password}
+                onChange={this.handleChange} autoComplete="password" />
+              <div class="invalid-feedback">
+                Please provide a password.
+              </div>
             </div>
           </div>
           <br></br>
           <div className="mb-3 row">
             <div className="col text-center">
-            <button className="btn button-custom me-2" type="submit">Register</button>
-            <button className="btn btn-secondary ms-2" onClick={this.cancel}>Cancel</button>
+              <button className="btn button-custom me-2" type="submit">Register</button>
+              <button className="btn btn-secondary ms-2" onClick={this.cancel}>Cancel</button>
             </div>
           </div>
         </form>
