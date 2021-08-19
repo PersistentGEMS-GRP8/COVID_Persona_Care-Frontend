fef116cd20389680f7c00cf59dee4a1bdf92d09f
Aksshar Ramesh <aksshar_ramesh@persistent.com>
2021-08-19 23:48:19 +0530

    test updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,26 +1,16 @@
 import { render } from '@testing-library/react';
 import Enzyme, { mount } from 'enzyme';
 import App from './App';
-import Quotes from './components/Quotes';
 
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-test('displays a quote', () => {
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders login component', () => {
   render(<App />);
-  const quote = document.querySelector('#text p');
-  expect(quote).toBeInTheDocument();
-  expect(quote).not.toBeEmptyDOMElement();
-});
-
-it('calls generateRandomQuote prop function when next button is clicked', () => {
-  const generateRandomQuoteFn = jest.fn();
-  const quote = mount(
-    <Quotes generateRandomQuote={generateRandomQuoteFn} quote={{}} />
-  );
-  const generateBtn = quote.find('#new-quote');
-
-  generateBtn.simulate('click');
-  expect(generateRandomQuoteFn).toHaveBeenCalledTimes(1);
+  const linkElement = screen.getByText(/Home/i);
+  expect(linkElement).toBeInTheDocument();
 });
