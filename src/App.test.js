47f8fcdfd4217c3c793d01dcb063d12387c8929b
Aksshar Ramesh <aksshar_ramesh@persistent.com>
2021-08-19 23:40:47 +0530

    package json updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react';
 import Enzyme, { mount } from 'enzyme';
+import App from './App';
+import Quotes from './components/Quotes';
 
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
