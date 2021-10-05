a6151880c09cb26b01e3c8283d3b92dc4a8ce0c4
sulakshana_ranawake <sula.ranawake@gmail.com>
2021-10-06 00:07:16 +0530

    Patient List displayed

--- src/components/Receptionist/ManagePatients/PatientList.jsx
+++ src/components/Receptionist/ManagePatients/PatientList.jsx
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 import PatientService from '../../../services/PatientService';
+import ReceptionistDashboard from '../ReceptionistDashboard';
 
 class PatientList extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            patients: [
-                {id:0,name:"S",email:"a@b.com",contact:"1233",vStatus:"YES"},
-                {id:1,name:"D",email:"a@b.com",contact:"1233",vStatus:"YES"}] 
+            patients: [] 
         }
+        this.editPatient = this.editPatient.bind(this);
+        this.removePatient = this.removePatient.bind(this);
     }
 
     editPatient(id) {
-        console.log(id);
-        this.props.history.push(`/patient/edit/${id}`);
+        this.props.history.push(`/patients/${id}`);
     }
 
     removePatient(id){
-        console.log(id);
+        PatientService.deletePatient(id).then( () => {
+            let updatedPatients = [...this.state.patients].filter(i => i.id !== id);
+            this.setState({patients: updatedPatients});
+        })
     }
 
     componentDidMount() {
@@ -31,19 +34,18 @@ class PatientList extends Component {
 
 
     render() {
-        const patients = this.state.patients;
-        // add {patients}
+        const {patients} = this.state;
 
         const patientList = patients.map(patient => {
             return <tr key={patient.id}>
             <td>{patient.id}</td>
             <td>{patient.name}</td>
             <td>{patient.email}</td>
-            <td>{patient.contact}</td>
-            <td>{patient.vStatus}</td>
+            <td>{patient.contactNo}</td>
+            <td>{patient.vaccinationStatus}</td>
             <td>
                 <div className="btn-group">
-                    <button className="btn button-custom btn-sm" onClick={ () => this.editPatient(patient.id)}>Edit</button>
+                    <button className="btn button-custom btn-sm" onClick={() => this.editPatient(patient.id)}>Edit</button>
                     <button className="btn button-delete btn-sm" onClick={() => this.removePatient(patient.id)}>Delete</button>
                 </div>
             </td>
@@ -52,6 +54,7 @@ class PatientList extends Component {
 
         return (
             <div>
+                <ReceptionistDashboard/>
                 <div className="container">
                     <table className="table table-hover">
                         <thead>
@@ -61,7 +64,7 @@ class PatientList extends Component {
                         <th width="20%">Email</th>
                         <th width="20%">Contact No</th>
                         <th width="10%">Vaccinated</th>
-                        <th width="10%"></th>
+                        <th width="10%">Actions</th>
                         </tr>
                         </thead>
                         <tbody>
